d9de4571c70c912cfade9bdd717e0e1c3d7708e5
sirbobz <bobcertified@gmail.com>
2018-06-19 19:46:54 +0300

    updated the scan base package

--- src/main/java/ke/co/statum/SortAlgorithApplication.java
+++ src/main/java/ke/co/statum/SortAlgorithApplication.java
@@ -1,9 +1,12 @@
 package ke.co.statum;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ke.co.statum")
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class SortAlgorithApplication {
 
 	public static void main(String[] args) {
