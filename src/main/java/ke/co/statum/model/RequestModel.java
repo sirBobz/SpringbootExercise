85ca59d047635430d0b106a074b429ef2d9a2be2
sirbobz <bobcertified@gmail.com>
2018-06-19 20:17:06 +0300

    created the model package and request model class with the string object

--- src/main/java/ke/co/statum/model/RequestModel.java
+++ src/main/java/ke/co/statum/model/RequestModel.java
@@ -2,4 +2,26 @@ package ke.co.statum.model;
 
 public class RequestModel {
 
+	private String string;
+
+	public RequestModel() {
+
+	}
+
+	public RequestModel(String string) {
+		this.string = string;
+	}
+
+	public String getString() {
+		return string;
+	}
+
+	public void setString(String string) {
+		this.string = string;
+	}
+
+	@Override
+	public String toString() {
+		return "RequestService [string=" + string + "]";
+	}
 }
--- src/main/java/ke/co/statum/service/RequestService.java
+++ src/main/java/ke/co/statum/service/RequestService.java
@@ -5,4 +5,35 @@ import org.springframework.stereotype.Service;
 @Service
 public class RequestService {
 
+	private String string;
+
+	public RequestService() {
+
+	}
+
+	public RequestService(String string) {
+		this.string = string;
+	}
+
+	public String getString() {
+		return string;
+	}
+
+	public void setString(String string) {
+		this.string = string;
+	}
+
+	@Override
+	public String toString() {
+		return "RequestService [string=" + string + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
