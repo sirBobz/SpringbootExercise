2137f46b821ba8b65452e53fafdca5030c261af3
sirbobz <bobcertified@gmail.com>
2018-06-19 19:42:36 +0300

    first commit

---
+++ src/main/java/ke/co/statum/SortAlgorithApplication.java
@@ -0,0 +1,12 @@
+package ke.co.statum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortAlgorithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortAlgorithApplication.class, args);
+	}
+}
