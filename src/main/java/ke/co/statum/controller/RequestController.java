79b0d64840c85f657d85831212a0d9d2471fa3c2
sirbobz <bobcertified@gmail.com>
2018-06-21 22:01:04 +0300

    final update on the request controller

--- src/main/java/ke/co/statum/controller/RequestController.java
+++ src/main/java/ke/co/statum/controller/RequestController.java
@@ -4,8 +4,6 @@
  */
 package ke.co.statum.controller;
 
-import javax.validation.Valid;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
