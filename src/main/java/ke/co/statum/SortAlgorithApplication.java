0d513c0245655afbe2bc6baebfee910bb9a46e80
sirbobz <bobcertified@gmail.com>
2018-06-21 22:29:10 +0300

    cleaned up on the unused import

--- src/main/java/ke/co/statum/SortAlgorithApplication.java
+++ src/main/java/ke/co/statum/SortAlgorithApplication.java
@@ -1,12 +1,9 @@
 package ke.co.statum;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication(scanBasePackages = "ke.co.statum")
-@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class SortAlgorithApplication {
 
 	public static void main(String[] args) {
